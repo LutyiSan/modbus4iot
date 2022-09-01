950c98a04c50d4bf9a0e6df9fd28bbf9fa94f017
Александр Луценко <avl@nppelement.com>
2022-09-01 21:01:52 +0300

    Start version-2

---
+++ gtw-v2/grouper.py
@@ -0,0 +1,120 @@
+from operator import itemgetter
+from env import MULTI_READ
+from csv_validator import check_csv_data
+
+PREFIX = "devices/"
+
+
+def csv_to_dict(csv_file, csv_delimiter):
+    with open(f"{PREFIX}{csv_file}", 'r') as fl:
+        csv_txt = fl.read().splitlines()
+    cols = csv_txt[0].split(csv_delimiter)
+    result_dict = dict.fromkeys(cols)
+    for col in cols:
+        result_dict[col] = []
+    idx = 0
+    rows = len(csv_txt) - 1
+    while idx < rows:
+        idx += 1
+        row = csv_txt[idx].split(csv_delimiter)
+        c = -1
+        for col in cols:
+            c += 1
+            if row[c] == '':
+                row[c] = 'None'
+            elif row[c].isdigit():
+                row[c] = int(row[c])
+            result_dict[col].append(row[c])
+    if check_csv_data(result_dict):
+        return result_dict
+    else:
+        return False
+
+
+def get_signal_list(device):
+    ldt = []
+    count = len(device['reg_address']) - 1
+    idx = -1
+    while idx < count:
+        idx += 1
+
+        dt = (
+            device['name'][idx], device['reg_type'][idx], device['reg_address'][idx], device['quantity'][idx],
+            device['bit_number'][idx],
+            device['value_type'][idx], device['scale'][idx])
+        ldt.append(dt)
+    # print(ldt)
+    return ldt
+
+
+def get_sorted_signal_dict(signals_list):
+    signals = {"reg_type": [], 'reg_address': [], 'quantity': [], 'bit_number': [], 'value_type': [], 'scale': [],
+               'name': []}
+    sl = sorted(signals_list, key=itemgetter(1, 2, 4))
+    for i in sl:
+        signals['reg_type'].append(i[1])
+        signals['reg_address'].append(i[2])
+        signals['quantity'].append(i[3])
+        signals['bit_number'].append(i[4])
+        signals['value_type'].append(i[5])
+        signals['scale'].append(i[6])
+        signals['name'].append(i[0])
+    # print(signals)
+    return signals
+
+
+class Grouper:
+    def __init__(self, device):
+        self.return_dict = {'start_address': [], 'read_quantity': [], 'reg_type': [], 'reg_address': [],
+                            'quantity': [], 'value_type': [], 'bit_number': [], 'name': [], 'present_value': [],
+                            'scale': []}
+        self.signals = csv_to_dict(device, ';')
+        if self.signals:
+            self.signals = get_sorted_signal_dict(get_signal_list(self.signals))
+            self.signals['start_address'] = []
+            self.signals['read_quantity'] = []
+            self.signals['present_value'] = []
+
+    def grouping(self):
+        if self.signals:
+            if MULTI_READ > 125:
+                max_query = 125
+            else:
+                max_query = MULTI_READ
+            self.len_signals = len(self.signals['reg_address']) - 2
+            self.start_address = self.signals['reg_address'][0]
+            self.read_quantity = 0
+            i = -1
+            while i < self.len_signals:
+                if self.read_quantity == max_query:
+                    self.__append_group(self.start_address, self.read_quantity)
+                    self.start_address = self.signals['reg_address'][i + 1]
+                    self.read_quantity = 0
+                i += 1
+                if self.signals['reg_type'][i] == self.signals['reg_type'][i + 1]:
+                    self.__type_grouper(i)
+                else:
+                    self.__append_group(self.start_address, self.read_quantity)
+                    self.__type_grouper(i)
+            self.__append_group(self.start_address, self.read_quantity)
+
+            return self.signals
+        else:
+            return False
+
+    def __append_group(self, *args):
+        self.signals['start_address'].append(args[0])
+        self.signals['read_quantity'].append(args[1])
+
+    def __type_grouper(self, i):
+        if self.signals['reg_address'][i] + self.signals['quantity'][i] == self.signals['reg_address'][i + 1]:
+            self.read_quantity += self.signals['quantity'][i + 1]
+        elif (self.signals['reg_address'][i] + self.signals['quantity'][i] != self.signals['reg_address'][
+            i + 1]) \
+                and self.signals['reg_address'][i] != self.signals['reg_address'][i + 1]:
+            if self.read_quantity == 0:
+                self.read_quantity = self.signals['quantity'][i]
+            self.__append_group(self.start_address, self.read_quantity)
+            self.start_address = self.signals['reg_address'][i + 1]
+            self.read_quantity = self.signals['quantity'][i + 1]
+
---
+++ gtw-v2/modbus_easymodbus_.py
@@ -0,0 +1,135 @@
+from easymodbus import modbusClient
+from loguru import logger
+from func_timeout import func_set_timeout
+from gtw.env import READ_TIMEOUT
+
+FAULT_VALUE = 'fault'
+
+
+class TCPClient:
+    def __init__(self, ip_address, tcp_port):
+        self.client = modbusClient.ModbusClient(ip_address, tcp_port)
+
+    def connection(self):
+        try:
+            self.client.connect()
+            logger.debug("Modbus READY connection")
+            return True
+        except Exception as e:
+            logger.exception("FAIL connect to device\n", e)
+            return False
+
+    @func_set_timeout(READ_TIMEOUT)
+    def read_single(self, device):
+        device['present_value'] = []
+        pv_list = []
+        logger.info("Start reading ......")
+        query_count = len(device['name']) - 1
+        idx = -1
+        while idx < query_count:
+            idx += 1
+            if device['reg_type'][idx] == "hr":
+                pv = read_verifier(self.read_hr(device['reg_address'][idx], device['quantity'][idx]),
+                                   device['quantity'][idx])
+                pv_list.append(pv)
+            elif device['reg_type'][idx] == "ir":
+                pv = read_verifier(self.read_ir(device['reg_address'][idx], device['quantity'][idx]),
+                                   device['quantity'][idx])
+                pv_list.append(pv)
+            elif device['reg_type'][idx] == "co":
+                pv = read_verifier(self.read_coils(device['reg_address'][idx], device['quantity'][idx]),
+                                   device['quantity'][idx])
+                pv_list.append(pv)
+            elif device['reg_type'][idx] == "di":
+                pv = read_verifier(self.read_di(device['reg_address'][idx], device['quantity'][idx]),
+                                   device['quantity'][idx])
+                pv_list.append(pv)
+        logger.info(".....STOP reading")
+        data_list = []
+        for i in pv_list:
+            for ii in i:
+                data_list.append(ii)
+        return device, data_list
+
+    @func_set_timeout(READ_TIMEOUT)
+    def read_multiple(self, signals):
+        pv_list = []
+        data_list = []
+        count = len(signals['start_address'])
+        idx = -1
+        while idx < count - 1:
+            idx += 1
+            if signals['reg_type'][idx] == "hr":
+                pv = read_verifier(self.read_hr(signals['start_address'][idx], signals['read_quantity'][idx]),
+                                   signals['read_quantity'][idx])
+                pv_list.append(pv)
+            elif signals['reg_type'][idx] == "ir":
+                pv_list.append(
+                    read_verifier(self.read_ir(signals['start_address'][idx], signals['read_quantity'][idx]),
+                                  signals['read_quantity'][idx]))
+            elif signals['reg_type'][idx] == "coil":
+                pv_list.append(
+                    read_verifier(self.read_coils(signals['start_address'][idx], signals['read_quantity'][idx]),
+                                  signals['read_quantity'][idx]))
+            elif signals['reg_type'][idx] == "di":
+                pv_list.append(
+                    read_verifier(self.read_di(signals['start_address'][idx], signals['read_quantity'][idx]),
+                                  signals['read_quantity'][idx]))
+        # logger.info(f"Reading list{pv_list}")
+        logger.info(".....STOP reading")
+        for i in pv_list:
+            for ii in i:
+                data_list.append(ii)
+        return signals, data_list
+
+    def read_hr(self, reg_number, quantity):
+        try:
+            result = self.client.read_holdingregisters(reg_number, quantity)
+
+            return result
+        except Exception as e:
+            logger.exception("Can't Read registers\n", e)
+            return False
+
+    def read_ir(self, reg_number, quantity):
+        try:
+            result = self.client.read_inputregisters(reg_number, quantity)
+            return result
+        except Exception as e:
+            logger.exception("Can't Read registers\n", e)
+            return False
+
+    def read_coils(self, reg_number, quantity):
+        try:
+            result = self.client.read_coils(reg_number, quantity)
+            return result
+        except Exception as e:
+            logger.exception("Can't Read registers\n", e)
+            return False
+
+    def read_di(self, reg_number, quantity):
+        try:
+            result = self.client.read_coils(reg_number, quantity)
+            return result
+        except Exception as e:
+            logger.exception("Can't Read registers\n", e)
+            return False
+
+    def disconnect(self):
+        try:
+            self.client.close()
+            logger.debug("Connection closed")
+        except Exception as e:
+            logger.exception("Can't close connection", e)
+
+
+def read_verifier(values, quantity):
+    return_values = []
+    if isinstance(values, list) and (len(values) == quantity):
+        logger.info(values)
+        return values
+    else:
+        for i in range(quantity):
+            return_values.append(FAULT_VALUE)
+        logger.info(return_values)
+        return return_values
